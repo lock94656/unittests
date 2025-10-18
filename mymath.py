b5b0c07e49718c75dc11a514ec5ddbf91ef565f3
lock94656 <gibert.hug@gmail.com>
2025-10-18 17:01:22 +0200

    Documentation of the code

--- mymath.py
+++ mymath.py
@@ -1,4 +1,16 @@
 def abs(value):
+	"""Computes the absolute value of a given value
+
+	Parameters
+	------------
+	value: float
+		the input float value
+	
+	Returns
+	--------
+	value or -value
+		the absolute value of a given value
+	"""
 	if value >= 0:
 		return value
 	elif value < 0:
--- testmymath.py
+++ testmymath.py
@@ -2,17 +2,21 @@ import mymath
 import unittest
 
 class TestMyMath(unittest.TestCase):
+	"""Test absolute function for different values"""
 	def test_upper(self):
+		"""Test for values greater than 0"""
 		self.assertEqual(abs(2.1), 2.1)
 		self.assertEqual(abs(5.2), 5.2)
 		self.assertEqual(abs(789), 789)
 		
 	def test_lower(self):
+		"""Test for values lower than 0"""
 		self.assertEqual(abs(-2.58), 2.58)
 		self.assertEqual(abs(-3.56), 3.56)
 		self.assertEqual(abs(-789), 789)
 		
 	def test_zero(self):
+		"""Test for values equal to 0"""
 		self.assertEqual(abs(0), 0)
 		self.assertEqual(abs(0.0), 0.0)
 		self.assertEqual(abs(-0.0), 0.0)
